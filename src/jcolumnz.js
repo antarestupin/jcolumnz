a50a33e26a096e1ce1ac15cdcfb9e01f36c95090
MARTIN Damien <damien@martin-damien.fr>
2014-02-04 17:13:48 +0100

    Working on

--- src/jcolumnz.js
+++ src/jcolumnz.js
@@ -7,6 +7,63 @@
 
 (function( $ ) {
 
+    /**
+     * Explore the data tree recursively extracting elements.
+     *
+     * @todo Change iteration (for..of is only accepted by Firefox).
+     * @param container
+     * @param root
+     */
+    function jcolumnz_exploreTree( container, root ) {
+
+        var elements = [];
+
+        for ( entry of root ) {
+
+            console.log(entry);
+
+            if ( typeof entry.children !== "undefined" ) {
+
+                jcolumnz_exploreTree( container, entry.children );
+
+            }
+
+            elements.push( entry );
+
+        }
+
+        jcolumnz_createNewPanel( container, elements );
+
+    }
+
+    /**
+     * Create a new panel inside the main container.
+     *
+     * @todo Change iteration (for..of is only accepted by Firefox).
+     * @param container
+     * @param elements
+     */
+    function jcolumnz_createNewPanel( container, elements ) {
+
+        console.log("New Panel >> " + elements);
+
+        // Build <li>s.
+
+        var li = "";
+        for ( element of elements ) {
+            li += "<li>" + element.label + "</li>";
+        }
+
+        container.append('<div class="jcolumnz-panel"><ul>' + li + '</ul></div>');
+
+    }
+
+    /**
+     * Function accessible through the user's code.
+     *
+     * @param options
+     * @returns {$.fn}
+     */
     $.fn.columnz = function( options ) {
 
         // Load options and save them into 'settings'.
@@ -18,6 +75,8 @@
         // Add a class to our container for easy styling.
 
         $(this).addClass("jcolumnz-container");
+
+        jcolumnz_exploreTree( $(this), settings.data )
 
         return this;
 
