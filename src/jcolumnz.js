f99ba3143fdaa6a7fae0f758294668e122afd5e0
supersaucisse <antares993@gmail.com>
2014-03-21 15:15:36 +0100

    Version finale

--- src/jcolumnz.js
+++ src/jcolumnz.js
@@ -22,8 +22,8 @@
 
             if ( typeof element.children !== "undefined" ) {
 
-				element.parent = id_parent;
-				element.id = id;
+                element.parent = id_parent;
+                element.id = id;
                 element.target = exploreTree( id, $container, element.children );
 
             }
@@ -51,8 +51,8 @@
         // Build <ul>.
 
         var $ul = $(document.createElement('ul')).addClass('jColumnz-list'),
-			$li;
-		
+            $li;
+        
         for ( var i=0, element ; element = elements[i] ; i++ ) {
 
             id = Math.random().toString(36).substr(2, 9);
@@ -63,42 +63,42 @@
 
             element.on_click && $li.click(element.on_click);
 
-			// Add tree system
-			
-			if ( element.target ) {
+            // Add tree system
+            
+            if ( element.target ) {
 
-				$li.addClass('jcolumnz-parent');
-				
-				$li.click((function(element){
-					return function(){
-						// Add and remove active class
-						$container.find('.jcolumnz-line').removeClass('jcolumnz-active');
-						$(this).addClass('jcolumnz-active');
-						
-						// Add and remove last class
-						$container.find('.jcolumnz-panel').removeClass('jcolumnz-last');
-						$('.jcolumnz-panel-'+ element.target).addClass('jcolumnz-last');
-						
-						// Hide every panel, excepted the one clicked, its parent and the children's panel
-						var not_parent = element.parent ? ':not(.jcolumnz-panel-'+ element.parent +')' : '';
-						$container.find(
-							'.jcolumnz-panel' +
-							':not(.jcolumnz-panel-'+ element.id +')' +
-							not_parent +
-							':not(.jcolumnz-panel-'+ element.target +')'
-						).hide(400);
-						
-						// Show needed panels (son and parent)
-						$container.find('.jcolumnz-panel-'+ element.target +', .jcolumnz-panel-'+ element.parent).show(400);
-					}
-				}(element)));
+                $li.addClass('jcolumnz-parent');
+                
+                $li.click((function(element){
+                    return function(){
+                        // Add and remove active class
+                        $container.find('.jcolumnz-line').removeClass('jcolumnz-active');
+                        $(this).addClass('jcolumnz-active');
+                        
+                        // Add and remove last class
+                        $container.find('.jcolumnz-panel').removeClass('jcolumnz-last');
+                        $('.jcolumnz-panel-'+ element.target).addClass('jcolumnz-last');
+                        
+                        // Hide every panel, excepted the one clicked, its parent and the children's panel
+                        var not_parent = element.parent ? ':not(.jcolumnz-panel-'+ element.parent +')' : '';
+                        $container.find(
+                            '.jcolumnz-panel' +
+                            ':not(.jcolumnz-panel-'+ element.id +')' +
+                            not_parent +
+                            ':not(.jcolumnz-panel-'+ element.target +')'
+                        ).hide(400);
+                        
+                        // Show needed panels (son and parent)
+                        $container.find('.jcolumnz-panel-'+ element.target +', .jcolumnz-panel-'+ element.parent).show(400);
+                    }
+                }(element)));
 
-			}
-			else {
-				$li.addClass('jcolumnz-final');
-			}
-			
-			$ul.append($li);
+            }
+            else {
+                $li.addClass('jcolumnz-final');
+            }
+            
+            $ul.append($li);
 
         }
 
@@ -121,12 +121,12 @@
         }, options );
 
         // Add a class to our container for easy styling.
-		
+        
         this.addClass("jcolumnz-container");
 
         var first = exploreTree( null, this, settings.data );
-		
-		this.find('.jcolumnz-panel:not(.jcolumnz-panel-'+ first +')').hide();
+        
+        this.find('.jcolumnz-panel:not(.jcolumnz-panel-'+ first +')').hide();
 
         return this;
 
